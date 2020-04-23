1d1b3272fec1576f4b048bf730eaf95feb1b88e8
Angel Gutierrez <gutierrez.monola@gmail.com>
2020-04-22 19:49:37 -0500

    Finished development

--- db/init/001_create_database.sh
+++ db/init/001_create_database.sh
@@ -2,6 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE develop;
-    CREATE DATABASE testing;
+    CREATE DATABASE demo_development;
+    CREATE DATABASE demo_testing;
 EOSQL
